a227414f664d6bac9e95b69dea1f8b08fe1d3fbd
Matija Mašanović <matijamasanovic01@gmail.com>
2026-07-12 16:49:46 +0200

    kotor picture updated

--- lib/tours-data.ts
+++ lib/tours-data.ts
@@ -16,7 +16,7 @@ export const TOURS: Tour[] = [
     price: 200,
     duration: "8h",
     image:
-      "https://images.unsplash.com/photo-1555990793-da11153b2473?w=800&q=80",
+      "https://images.unsplash.com/photo-1614122027743-50a9e6e8002f?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: "biogradska-gora",
