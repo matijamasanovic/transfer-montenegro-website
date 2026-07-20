50b0fc3c80c63352946317131ea062d38e794c6b
Matija Mašanović <matijamasanovic01@gmail.com>
2026-07-20 15:45:23 +0200

    change default locale to en

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["cnr", "en", "ru"],
-  defaultLocale: "cnr",
+  defaultLocale: "en",
   localePrefix: "as-needed",
   localeDetection: false,
 });
